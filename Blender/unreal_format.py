d8aa9509faf8af61cbd2ca679a3d7bf1e8a8024f
halfuwu <maxman1346@gmail.com>
2023-08-30 22:26:14 -0400

    start anim support

--- Blender/unreal_format.py
+++ Blender/unreal_format.py
@@ -34,7 +34,9 @@ class UFPanel(bpy.types.Panel):
     
     def draw(self, context):
         UFPanel.draw_model_options(self, context)
-        self.layout.row().operator("uf.import_umodel", icon='MESH_DATA')
+        self.layout.row().operator("uf.import_uemodel", icon='MESH_DATA')
+        UFPanel.draw_anim_options(self, context)
+        self.layout.row().operator("uf.import_ueanim", icon='ANIM')
        
     @staticmethod
     def draw_model_options(self, context):
@@ -46,10 +48,19 @@ class UFPanel(bpy.types.Panel):
         box.row().prop(bpy.context.scene.uf_settings, "bone_length")
         box.row().prop(bpy.context.scene.uf_settings, "scale")
         
-class UFImportUModel(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
-    bl_idname = "uf.import_umodel"
+    @staticmethod
+    def draw_anim_options(self, context):
+        layout = self.layout
+
+        box = layout.box()
+        box.label(text="Anim Options", icon="ACTION")
+        box.row().prop(bpy.context.scene.uf_settings, "rotation_only")
+        
+class UFImportUEModel(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
+    bl_idname = "uf.import_uemodel"
     bl_label = "Import Model"
     bl_context = 'scene'
+    
     filename_ext = ".uemodel"
     filter_glob: StringProperty(default="*.uemodel", options={'HIDDEN'}, maxlen=255)
     files: CollectionProperty(type=bpy.types.OperatorFileListElement, options={'HIDDEN', 'SKIP_SAVE'})
@@ -62,16 +73,36 @@ class UFImportUModel(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
 
     def draw(self, context):
        UFPanel.draw_model_options(self, context)
+       
+class UFImportUEAnim(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
+    bl_idname = "uf.import_ueanim"
+    bl_label = "Import Animation"
+    bl_context = 'scene'
+    
+    filename_ext = ".ueanim"
+    filter_glob: StringProperty(default="*.ueanim", options={'HIDDEN'}, maxlen=255)
+    files: CollectionProperty(type=bpy.types.OperatorFileListElement, options={'HIDDEN', 'SKIP_SAVE'})
+    directory: StringProperty(subtype='DIR_PATH')
+    
+    def execute(self, context):
+        for file in self.files:
+            import_file(os.path.join(self.directory, file.name))
+        return {'FINISHED'}
+
+    def draw(self, context):
+       UFPanel.draw_anim_options(self, context)
     
 class UFSettings(bpy.types.PropertyGroup):
     scale: FloatProperty(name="Scale", default=0.01, min = 0.01)
     bone_length: FloatProperty(name="Bone Length", default=5, min = 0.1)
     reorient_bones: BoolProperty(name="Reorient Bones", default=False)
+    rotation_only: BoolProperty(name="Rotation Only", default=False)
     
 def draw_import_menu(self, context):
-    self.layout.operator(UFImportUModel.bl_idname, text = "Unreal Model (.uemodel)")
+    self.layout.operator(UFImportUEModel.bl_idname, text = "Unreal Model (.uemodel)")
+    self.layout.operator(UFImportUEAnim.bl_idname, text = "Unreal Animation (.ueanim)")
 
-operators = [UFPanel, UFImportUModel, UFSettings]
+operators = [UFPanel, UFImportUEModel, UFImportUEAnim, UFSettings]
 
 def register():
     for operator in operators:
@@ -159,14 +190,20 @@ class FArchiveReader:
 
     def skip(self, size: int):
         self.data.read(size)
+        
+    def read_bulk_array(self, predicate):
+        count = self.read_int()
+        return self.read_array(count, predicate)
 
-    def read_bulk_array(self, count: int, predicate):
-        bulk_array = []
+    def read_array(self, count, predicate):
+        array = []
         for counter in range(count):
-            bulk_array.append(predicate(self))
-        return bulk_array
+            array.append(predicate(self))
+        return array
+    
+    
 
-class UModel:
+class UEModel:
     vertices = []
     indices = []
     normals = []
@@ -218,8 +255,7 @@ class MorphTarget:
     def __init__(self, ar: FArchiveReader):
         self.name = ar.read_fstring()
 
-        count = ar.read_int()
-        self.deltas = ar.read_bulk_array(count, lambda archive: MorphTargetData(archive))
+        self.deltas = ar.read_bulk_array(lambda ar: MorphTargetData(ar))
 
 class MorphTargetData:
     position = []
@@ -244,10 +280,35 @@ class Socket:
         self.position = [pos * bpy.context.scene.uf_settings.scale for pos in ar.read_float_vector(3)]
         self.rotation = ar.read_float_vector(4)
         self.scale = ar.read_float_vector(3)
+        
+class UEAnim:
+    num_frames = 0
+    frames_per_second = 0
+    tracks = []
+    
+class Track:
+    name = ""
+    keys = []
+
+    def __init__(self, ar: FArchiveReader):
+        self.name = ar.read_fstring()
+        self.keys = ar.read_bulk_array(lambda ar: BoneKey(ar))
+        
+class BoneKey:
+    position = []
+    rotation = []
+    scale = []
+
+    def __init__(self, ar: FArchiveReader):
+        self.position = [pos * bpy.context.scene.uf_settings.scale for pos in ar.read_float_vector(3)]
+        self.rotation = ar.read_float_vector(4)
+        self.scale = ar.read_float_vector(3)
+        
 # ---------- IMPORT FUNCTIONS ---------- #
 
 MAGIC = "UNREALFORMAT"
-MODEL_INDENTIFIER = "UEMODEL"
+MODEL_IDENTIFIER = "UEMODEL"
+ANIM_IDENTIFIER = "UEANIM"
 
 def import_file(path: str):
     with open(path, 'rb') as file:
@@ -282,10 +343,13 @@ def import_data(data, link_model: bool = True):
                 return
 
 
-        if identifier == MODEL_INDENTIFIER:
-            return import_umodel_data(read_archive, object_name, link_model)
-def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
-    umodel = UModel()
+        if identifier == MODEL_IDENTIFIER:
+            return import_uemodel_data(read_archive, object_name, link_model)
+        elif identifier == ANIM_IDENTIFIER:
+            return import_ueanim_data(read_archive, object_name)
+        
+def import_uemodel_data(ar: FArchiveReader, name: str, link: bool):
+    data = UEModel()
 
     while not ar.eof():
         header_name = ar.read_fstring()
@@ -293,33 +357,33 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
         byte_size = ar.read_int()
 
         if header_name == "VERTICES":
-            umodel.vertices = ar.read_bulk_array(array_size, lambda ar: [vert * bpy.context.scene.uf_settings.scale for vert in ar.read_float_vector(3)])
+            data.vertices = ar.read_array(array_size, lambda ar: [vert * bpy.context.scene.uf_settings.scale for vert in ar.read_float_vector(3)])
         elif header_name == "INDICES":
-            umodel.indices = ar.read_bulk_array(int(array_size / 3), lambda ar: ar.read_int_vector(3))
+            data.indices = ar.read_array(int(array_size / 3), lambda ar: ar.read_int_vector(3))
         elif header_name == "NORMALS":
-            umodel.normals = ar.read_bulk_array(array_size, lambda ar: ar.read_float_vector(3))
+            data.normals = ar.read_array(array_size, lambda ar: ar.read_float_vector(3))
         elif header_name == "TANGENTS":
-            umodel.tangents = ar.read_bulk_array(array_size, lambda ar: ar.read_float_vector(3))
+            data.tangents = ar.read_array(array_size, lambda ar: ar.read_float_vector(3))
         elif header_name == "VERTEXCOLORS":
-            umodel.colors = ar.read_bulk_array(array_size, lambda ar: ar.read_byte_vector(4))
+            data.colors = ar.read_array(array_size, lambda ar: ar.read_byte_vector(4))
         elif header_name == "TEXCOORDS":
-            umodel.uvs = ar.read_bulk_array(array_size, lambda ar: ar.read_bulk_array(ar.read_int(), lambda ar: ar.read_float_vector(2)))
+            data.uvs = ar.read_array(array_size, lambda ar: ar.read_array(ar.read_int(), lambda ar: ar.read_float_vector(2)))
         elif header_name == "MATERIALS":
-            umodel.materials = ar.read_bulk_array(array_size, lambda ar: Material(ar))
+            data.materials = ar.read_array(array_size, lambda ar: Material(ar))
         elif header_name == "WEIGHTS":
-            umodel.weights = ar.read_bulk_array(array_size, lambda ar: Weight(ar))
+            data.weights = ar.read_array(array_size, lambda ar: Weight(ar))
         elif header_name == "BONES":
-            umodel.bones = ar.read_bulk_array(array_size, lambda ar: Bone(ar))
+            data.bones = ar.read_array(array_size, lambda ar: Bone(ar))
         elif header_name == "MORPHTARGETS":
-            umodel.morphs = ar.read_bulk_array(array_size, lambda ar: MorphTarget(ar))
+            data.morphs = ar.read_array(array_size, lambda ar: MorphTarget(ar))
         elif header_name == "SOCKETS":
-            umodel.sockets = ar.read_bulk_array(array_size, lambda ar: Socket(ar))
+            data.sockets = ar.read_array(array_size, lambda ar: Socket(ar))
         else:
             ar.skip(byte_size)
 
     # geometry
     mesh_data = bpy.data.meshes.new(name)
-    mesh_data.from_pydata(umodel.vertices, [], umodel.indices)
+    mesh_data.from_pydata(data.vertices, [], data.indices)
 
     mesh_object = bpy.data.objects.new(name, mesh_data)
     return_object = mesh_object
@@ -327,27 +391,27 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
         bpy.context.scene.collection.objects.link(mesh_object)
 
     # normals
-    if len(umodel.normals) > 0:
+    if len(data.normals) > 0:
         mesh_data.polygons.foreach_set("use_smooth", [True] * len(mesh_data.polygons))
-        mesh_data.normals_split_custom_set_from_vertices(umodel.normals)
+        mesh_data.normals_split_custom_set_from_vertices(data.normals)
         mesh_data.use_auto_smooth = True
 
     # weights
-    if len(umodel.weights) > 0 and len(umodel.bones) > 0:
-        for weight in umodel.weights:
-            bone_name = umodel.bones[weight.bone_index].name
+    if len(data.weights) > 0 and len(data.bones) > 0:
+        for weight in data.weights:
+            bone_name = data.bones[weight.bone_index].name
             vertex_group = mesh_object.vertex_groups.get(bone_name)
             if not vertex_group:
                 vertex_group = mesh_object.vertex_groups.new(name = bone_name)
             vertex_group.add([weight.vertex_index], weight.weight, 'ADD')
 
     # morph targets
-    if len(umodel.morphs) > 0:
+    if len(data.morphs) > 0:
         default_key = mesh_object.shape_key_add(from_mix=False)
         default_key.name = "Default"
         default_key.interpolation = 'KEY_LINEAR'
 
-        for morph in umodel.morphs:
+        for morph in data.morphs:
             key = mesh_object.shape_key_add(from_mix=False)
             key.name = morph.name
             key.interpolation = 'KEY_LINEAR'
@@ -356,23 +420,23 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
                 key.data[delta.vertex_index].co += Vector(delta.position)
                 
     # vertex colors
-    if len(umodel.colors) > 0:
+    if len(data.colors) > 0:
         vertex_color = mesh_data.vertex_colors.new(name="COL0", do_init=False)
         for polygon in mesh_data.polygons:
             for vertex_index, loop_index in zip(polygon.vertices, polygon.loop_indices):
-                color = umodel.colors[vertex_index]
+                color = data.colors[vertex_index]
 
     # texture coordinates
-    if len(umodel.uvs) > 0:
-        for index, uvs in enumerate(umodel.uvs):
+    if len(data.uvs) > 0:
+        for index, uvs in enumerate(data.uvs):
             uv_layer = mesh_data.uv_layers.new(name="UV" + str(index))
             for polygon in mesh_data.polygons:
                 for vertex_index, loop_index in zip(polygon.vertices, polygon.loop_indices):
                     uv_layer.data[loop_index].uv = uvs[vertex_index]
 
     # materials
-    if len(umodel.materials) > 0:
-        for material in umodel.materials:
+    if len(data.materials) > 0:
+        for material in data.materials:
             if material.material_name == "":
                 mesh_data.materials.append(None)
                 continue
@@ -383,7 +447,7 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
             mesh_data.materials.append(mat)
 
     # skeleton
-    if len(umodel.bones) > 0 or len(umodel.sockets) > 0:
+    if len(data.bones) > 0 or len(data.sockets) > 0:
         armature_data = bpy.data.armatures.new(name=name)
         armature_data.display_type = 'STICK'
 
@@ -398,11 +462,11 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
 
         mesh_object.parent = armature_object
                 
-    if len(umodel.bones) > 0:
+    if len(data.bones) > 0:
         # create bones
         bpy.ops.object.mode_set(mode='EDIT')
         edit_bones = armature_data.edit_bones
-        for bone in umodel.bones:
+        for bone in data.bones:
             bone_pos = Vector(bone.position)
             bone_rot = Quaternion((bone.rotation[3], bone.rotation[0], bone.rotation[1], bone.rotation[2])) # xyzw -> wxyz
 
@@ -412,7 +476,7 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
             bone_matrix = Matrix.Translation(bone_pos) @ bone_rot.to_matrix().to_4x4()
 
             if bone.parent_index >= 0:
-                parent_bone = edit_bones.get(umodel.bones[bone.parent_index].name)
+                parent_bone = edit_bones.get(data.bones[bone.parent_index].name)
                 edit_bone.parent = parent_bone
                 bone_matrix = parent_bone.matrix @ bone_matrix
     
@@ -444,10 +508,10 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
         bpy.ops.object.mode_set(mode='OBJECT')
 
     # sockets
-    if len(umodel.sockets) > 0:
+    if len(data.sockets) > 0:
         # create sockets
         bpy.ops.object.mode_set(mode='EDIT')
-        for socket in umodel.sockets:
+        for socket in data.sockets:
             socket_bone = edit_bones.new(socket.name)
             parent_bone = edit_bones.get(socket.parent_name)
             socket_bone.parent = parent_bone
@@ -458,8 +522,25 @@ def import_umodel_data(ar: FArchiveReader, name: str, link: bool):
         # socket colors
         socket_group = armature_object.pose.bone_groups.new(name='Sockets')
         socket_group.color_set = 'THEME05'
-        for socket in umodel.sockets:
+        for socket in data.sockets:
             armature_object.pose.bones.get(socket.name).bone_group = socket_group
         bpy.ops.object.mode_set(mode='OBJECT')
 
     return return_object
+
+def import_ueanim_data(ar: FArchiveReader, name: str):
+    data = UEAnim()
+    
+    data.num_frames = ar.read_int()
+    data.frames_per_second = ar.read_float()
+
+    while not ar.eof():
+        header_name = ar.read_fstring()
+        array_size = ar.read_int()
+        byte_size = ar.read_int()
+
+        if header_name == "TRACKS":
+            data.tracks = ar.read_array(array_size, lambda ar: Track(ar))
+        else:
+            ar.skip(byte_size)
+    
